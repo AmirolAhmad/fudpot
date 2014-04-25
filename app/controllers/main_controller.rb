388d90e94848448635b4a2787ae8ec67469c534e
Amirol Ahmad <9w2pdf@gmail.com>
2014-04-25 23:04:18 +0800

    fixed the submit a recipe page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,6 @@ class MainController < ApplicationController
 	
   def index
   	@menus = Menu.paginate(page: params[:page])
-  	@menu = current_user.menus.build if signed_in?
   end
 
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,27 +2,27 @@ class MenusController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
 
   def index
-  	
+    @menus = Menu.paginate(page: params[:page])
+    @menu = current_user.menus.build
   end
 
   def create
-	@menu = current_user.menus.build(menu_params)
+    @menu = current_user.menus.build(menu_params)
     if @menu.save
       flash[:success] = "Recipe created!"
       redirect_to root_url
     else
-      render 'main/index'
+      render 'menus/index'
     end
   end
 
   def destroy
-
   end
 
   private
 
   def menu_params
-    params.require(:menu).permit(:description, :title, :ingredient)
+    params.require(:menu).permit(:title, :ingredient, :description)
   end
 
 end
