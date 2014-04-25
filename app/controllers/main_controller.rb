039a783a3a8317f498bd44f7473abd7c0726a762
AmirolAhmad <9w2pdf@gmail.com>
2014-04-25 14:58:53 +0800

    separate the create recipe from homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,11 @@
 class MainController < ApplicationController
-	
-  def index
-  	@menus = Menu.paginate(page: params[:page])
+	before_action :signed_in_user, only: [:create]
+
+  	def index
+  		@menus = Menu.paginate(page: params[:page])
+  	end
+
+  def create
   	@menu = current_user.menus.build if signed_in?
   end
 
