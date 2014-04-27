34317741975f1c7abc3bc9698d48288a62101351
Amirol Ahmad <9w2pdf@gmail.com>
2014-04-27 21:58:18 +0800

    failed - remember me and password reset

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,20 +8,36 @@ class SessionsController < ApplicationController
     end
   end
 
+  #def create
+  #	user = User.find_by(email: params[:session][:email].downcase)
+  #  if user && user.authenticate(params[:session][:password])
+  #    sign_in user
+  #    redirect_back_or user
+  #  else
+  #    flash.now[:danger] = 'Invalid email/password combination'
+  #    render 'new'
+  #  end
+  #end
+
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
+      redirect_to root_url, :notice => "Logged in!"
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      flash.now[:danger] = "Invalid email or password"
+      render "new"
     end
   end
 
   def destroy
-    sign_out
-    redirect_to root_url
+    #sign_out
+    cookies.delete(:auth_token)
+    redirect_to root_url, :warning => "Logged out!"
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
+  before_create { generate_token(:auth_token) }
 
 	validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -44,6 +45,19 @@ class User < ActiveRecord::Base
 
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
   end
 
   private
