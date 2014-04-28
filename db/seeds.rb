781863a2d1fdb1a0018371b32acbbd53e3d9f4fc
AmirolAhmad <9w2pdf@gmail.com>
2014-04-28 14:24:20 +0800

    level & category db:seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+levels = Level.create([{ description: 'Easy' }, { description: 'Super Easy' }, { description: 'Intermediate' }, { description: 'Hard' }, { description: 'Super Hard' }])
+categories = Category.create([{ description: 'Appetizers' }, { description: 'Beverages' }, { description: 'Beef' }, { description: 'Breads & Rolls' }, { description: 'Breakfast & Brunch' }, { description: 'Brownies, Bars & Candy' }, { description: 'Burgers, Brats & Dogs' }, { description: 'cake & Cheesecakes' }, { description: 'Cookies' }, { description: 'Desserts' }, { description: 'Dips, Spreads & Sauces' }, { description: 'Ethnic Recipes' }, { description: 'Fish & Seafood' }, { description: 'Grilling' }, { description: 'Healthy Eating' }, { description: 'Holiday Treats' }, { description: 'Kids Cuisine' }, { description: 'Main Dishes' }, { description: 'One Dish Meals' }, { description: 'Pasta & Pizza' }, { description: 'Pies' }, { description: 'Pork' }, { description: 'Poultry' }, { description: 'Salads' }, { description: 'Sandwiches & Wraps' }, { description: 'Sides' }, { description: 'Snacks' }, { description: 'Soups & Stews' }, { description: 'Vegetarian' }])
